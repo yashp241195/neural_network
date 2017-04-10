08d81d03532079865639d0395b2e6a451829ba9d
Yash <yashp241195@gmail.com>
2017-04-10 23:31:30 +0530

    Update neural_net.py

--- neural_net.py
+++ neural_net.py
@@ -1,93 +1,201 @@
 import numpy as np
-import time
 
 
-X = np.array(([3, 5], [5, 1], [10, 2]), dtype=float)
-Y = np.array(([75], [82], [93]), dtype=float)
+
+
+
+# Artificial Neural Network :  
+
+'''
+Suppose you want to predict your test score
+from input given as :
+
+    X = [Number of hours you study , Number of hours you sleep]
+    Y = [Test on score out of 100]
+
+X - Input :
+
+-> X = [Number of hours you study , Number of hours you sleep]
+
+Y - Output :
+    
+-> Y = [Test Score]
+
+'''
+
+X = np.array(([3, 5], [5, 1], [10, 2]), dtype = float)
+
+Y = np.array(([75], [82], [93]), dtype = float)
+
+'''
+
+Normalization by scaling our Data :
+
+X = X/max(X)
+
+Y = Y/max(Y), where max(Y) is given as 100
+
+'''
+
 
 X /= np.amax(X, axis=0)
 Y /= 100
 
-#print ('X = ', X, ' \n Y = ', Y)
+print '\nInput Matrix \n X = \n', X, ' \n\n \n Output Matrix\n Y = \n', Y
+
 
 
 class NeuralNetwork(object):
     def __init__(self):
         # Define HyperParameters
+
+        '''
+
+        Input Layer size = Number of input
+        variables
+        
+        In Our case, Number of input
+        variables are :
+        
+        1) Number of hours you study
+        2) Number of hours you sleep
+
+        input Layer size must be 2
+        
+        '''
+
         self.inputLayerSize = 2
-        self.outputLayerSize = 1
+  
+        '''
+
+        Hidden Layer Size I defined as 3,
+        means inside the hidden layer
+        there are 3 neurons 
+        
+        '''
+
         self.hiddenLayerSize = 3
 
-        # weight
-        self.W1 = np.random.rand(self.inputLayerSize,self.hiddenLayerSize)
-        self.W2 = np.random.rand(self.hiddenLayerSize,self.outputLayerSize)
+        '''
 
+        Output Layer Size is 1 means
+        there is only one output
+        
+        '''
+
+        self.outputLayerSize = 1
+
+        
+        '''
+
+        Weight matrix distribution is done by
+        Normal Distribution which means distribution
+        is bell shaped curve with mean value = 0
+        and variance = 1 which means sum of row can
+        vary between(-1,1) and implimented using numpy
+
+        np.random.randn method so that we can converge
+        a result to some particular value easily for
+        the given input which need to pridict
+
+        First Parameter :- rows in matrix
+
+        Second Parameter :- columns in matrix
+  
+        '''
+   
+        # Weight    
+
+        '''
+        W1 = Weight Matrix 1 which connects input Layer to the hidden Layer
+        '''
+        
+        self.W1 = np.random.randn(self.inputLayerSize, self.hiddenLayerSize)
+
+        '''
+        W2 = Weight Matrix 2 which connects hidden Layer to the output Layer
+        '''
+        
+        self.W2 = np.random.randn(self.hiddenLayerSize, self.outputLayerSize)
+
+    # Forward Propogation :
+
+    
     def forward(self, X):
+        
         # Propagate inputs through network
+        # Matrix(z2) = Matrix(X) * Matrix(W1)
+
         self.z2 = np.dot(X, self.W1)
-        self.a2 = self.sigmoid(self.z2)
+
+        # Applying Activation Function to each
+        # element of Matrix(z2) and storing answer
+        # inside a2
+        
+        self.a2 = self.activation_function(self.z2)
+
+        # Matrix(z3) =  Matrix(a2) * Matrix(W2)
+
         self.z3 = np.dot(self.a2 , self.W2)
-        yHat = self.sigmoid(self.z3)
+
+        # yHat is the predicted Output Matrix
+        # for given input Matrix(X) 
+        
+
+        yHat = self.activation_function(self.z3)
+
+        
         return yHat
 
-    def sigmoid(self, z):
+    
+    # Computing the cost Function
+
+    def costFunction(self, X, Y):
+
+        #Compute cost for given X,y, use weights already stored in class.
+        self.yHat = self.forward(X)
+        
+        J = 0.5*sum((Y-self.yHat)**2)
+
+        return J
+
+    # Computing dJ/dW1 and dJ/dW2 for minimization
+
+    def costFunctionPrime(self, X, y):
+        
+        #Compute derivative with respect to W and W2 for a given X and y:
+        self.yHat = self.forward(X)
+        
+        delta3 = np.multiply(-(y-self.yHat), self.activation_function_derrivative(self.z3))
+        # for matrix M if I write M.T it means Transpose
+        
+        dJdW2 = np.dot(self.a2.T, delta3)
+        
+        
+        delta2 = np.dot(delta3, self.W2.T) * self.activation_function_derrivative(self.z2)
+        
+        dJdW1 = np.dot(X.T, delta2)  
+        
+        return dJdW1, dJdW2
+
+
+    # Activation Function 
+    
+    def activation_function(self, z):
+        
         # Applying sigmoid activation function
-        return 1 / (1 + np.exp(-z))
+        return (1 / (1 + np.exp(-z)))
+
+    def activation_function_derrivative(self,z):
+        
+        #Gradient of sigmoid
+        return np.exp(-z)/((1+np.exp(-z))**2)
+
+    
 
 NN = NeuralNetwork()
 yHat = NN.forward(X)
-#print (yHat)
 
-# Total Weights in neural net
-
-TotalWeights = (NN.inputLayerSize*NN.hiddenLayerSize) + (NN.hiddenLayerSize*NN.outputLayerSize)
-MaxItr = 10 * TotalWeights
-
-
-#Uniform distribution of 1000 numbers between -10 to +10
-weightsToTry = np.linspace(-10,10,MaxItr)
-
-#Cost matrix with 100 zeros
-cost = np.zeros(MaxItr)
-
-#Applying Brute force Optimization
-
-#Optimize for one weight out of Total Weights W[0][0]
-
-for i in range(0,MaxItr):
-    NN.W1[0][0] = weightsToTry[i];
-    yHat = NN.forward(X)
-    cost[i]=0.5*sum((Y-yHat)**2)
-
-print Y-yHat
-
-# for nine weights we need 90^9 iterations
-
-# Due to high time Complexity this will not be the Solution
-
-# Optimizing Cost function
-# by using Partial derrivatives to predict the direction of searching Value of weight
-
-# d/dW2(cost) = d/dW2(0.5*sum((Y-yHat)**2))
-
-# we need sigmoid derrivative also
-# these must be added to class Neural Network 
-
-def sigmoidPrime(self, z):
-    # Applying sigmoid activation function
-    return np.exp(-z) / ((1 + np.exp(-z))**2)
-
-#derrivative of cost function
-def costFunctionPrime(self, X, Y):
-    #Compute derrivative wrt W1 and W2
-    self.yHat=self.forward(X)
-
-    delta3 = np.multiply(-(Y-self.yHat), self.sigmoidPrime(self.z3))
-    dJdW2 = np.dot(self.a2.T, delta3)
-
-    delta2 = np.multiply(delta3, self.W2.T)*self.sigmoidPrime(self.z3)
-    dJdW1 = np.dot(X.T, delta3)
-
-
-
-
+print '\n Forward Propogation Output Prediction Matrix(yHat) : \n',yHat
+print '\n\n Cost Matrix :\n 1/2 (Matrix(Y) - Matrix(yHat))**2 \n\n ',NN.costFunction(X,Y)
+print '\n\n Minimizing Cost Function by Gradient Descent '
