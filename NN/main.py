f01a7db0dd6b5cb05c0356aa82d7e6bb23f8543a
Yash <yashp241195@gmail.com>
2017-11-04 21:53:24 +0530

    Update main.py

--- NN/main.py
+++ NN/main.py
@@ -7,11 +7,9 @@ def main():
     from input given as :
         X = [Number of hours you study , Number of hours you sleep]
         Y = [Test on score out of 100]
-    X - Input :
-    -> X = [Number of hours you study , Number of hours you sleep]
-    Y - Output :
-        
-    -> Y = [Test Score]
+    X - Input : -> X = [Number of hours you study , Number of hours you sleep]
+    Y - Output : -> Y = [Test Score]
+
     '''
     
     X = np.array(([3, 5], [5, 1], [10, 2]), dtype = float)
@@ -19,7 +17,7 @@ def main():
     Y = np.array(([75], [82], [93]), dtype = float)
     
     '''
-    Normalization by scaling our Data :
+    Normalization by scaling our Data in range (-1,1) :
     X = X/max(X)
     Y = Y/max(Y), where max(Y) is given as 100
     '''
@@ -28,7 +26,7 @@ def main():
     X /= np.amax(X, axis=0)
     Y /= 100
     
-    #print('\nInput Matrix \n X = \n', X, ' \n\n \n Output Matrix\n Y = \n', Y)
+    # print('\nInput Matrix \n X = \n', X, ' \n\n \n Output Matrix\n Y = \n', Y)
     
     
 if __name__== "__main__":
